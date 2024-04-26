f9702c9ad25d3479b8c037029ad31fcf1300ee89
RobertGAllison <20rga09@gmail.com>
2024-04-25 19:59:48 -0700

    lab 5 push

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * index.js - Lab 5
+ * Author: Robert G. Allison
+ * Date: 4/25/2024
+ */
 
-// Constants
+//Variables
+var make = "Honda";
+var model = "Civic";
+var color = "Champagne";
+var year = 2004;
 
-// Functions
+//age variable calculation
+var age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " +age + " years<br>");
