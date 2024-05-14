b008c3a599f2b274a23493335dadbd0e7e289e42
RobertGAllison <20rga09@gmail.com>
2024-05-14 16:28:19 -0700

    lab 9 part 2

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -6,15 +6,27 @@
 */
 $("#challenges").append("<button id='button-challenge'>Make Crasy</button>");
 $("button-challenge").click(function(){
-    alert("I cannot make this any crasier");
+    // now add (or subtract) the "special" class to the section
+    $("#challenges").toggleClass("special");
+});
+$("#challenges").on("click", function(){
+    alert("Cant make it Crasier, dawg.")
 });
 
 $("#problems").append("<button id='button-challenge'>Make Funky</button>");
 $("button-challenge").click(function(){
-    alert("I cannot make this any funkier");
+    // now add (or subtract) the "special" class to the section
+    $("#problems").toggleClass("special");
+});
+$("#problems").on("click", function(){
+    alert("Cant make it Funkier, man.")
 });
 
 $("#results").append("<button id='button-challenge'>Make Cool</button>");
 $("button-challenge").click(function(){
-    alert("I cannot make this any cooler");
+    // now add (or subtract) the "special" class to the section
+    $("#results").toggleClass("special");
+});
+$("#results").on("click", function(){
+    alert("Cant make it Cooler, dude.")
 });
