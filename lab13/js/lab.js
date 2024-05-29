135ed568c22f01757681da3615accb65b66872af
RobertGAllison <20rga09@gmail.com>
2024-05-29 11:05:15 -0700

    lab12&13 pushes

---
+++ lab13/js/lab.js
@@ -0,0 +1,29 @@
+/**
+ * Robert G Allison
+ * Tuesday May 28th
+ * Javscript for Lab13
+ */
+
+function fizzBuzz() {
+    var output = "";
+    for (var i = 1; i <= 200; i++) {
+        var str = "";
+        if (i % 3 === 0) {
+            str += "Fizz";
+        }
+        if (i % 5 === 0) {
+            str += "Buzz";
+        }
+        if (i % 7 === 0) {
+            str += "Boom";
+        }
+        if (str === "") {
+            str = i.toString();
+        }
+        output += "<p>" + str + "</p>";
+    }
+    document.getElementById("output").innerHTML = output;
+    str += "Fizz";
+$("#output").append("<p>" + str + "</p>");
+}
+fizzBuzz();
